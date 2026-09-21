09d950c3fd58cee73ba29d2de2a9852508379993
MD SHAZID ISLAM <misla048@ucr.edu>
2026-09-20 19:34:24 -0700

    Time: 23 ms (27.41%) | Memory: 12.4 MB (87.89%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -18,15 +18,49 @@ class Solution(object):
                 res.append(copy.deepcopy(curr_res))
                 return
 
-            if i>=len(candidates) or curr_sum > target:
-                return
+            for j in range(i,len(candidates)):
 
-            curr_res.append(candidates[i])
-            dfs(i,curr_res, curr_sum+candidates[i])
-            curr_res.pop()
-            dfs(i+1,curr_res, curr_sum)
+                if curr_sum + candidates[j]> target:
+                    break
+                
+                curr_res.append(candidates[j])
+                dfs(j,curr_res, curr_sum+candidates[j])
+                curr_res.pop()
+
 
         
         dfs(0,[], 0)
 
         return res
+
+
+# import copy
+# class Solution(object):
+#     def combinationSum(self, candidates, target):
+#         """
+#         :type candidates: List[int]
+#         :type target: int
+#         :rtype: List[List[int]]
+#         """
+#         res = []
+
+
+#         def dfs(i,curr_res, curr_sum):
+
+
+#             if curr_sum == target:
+#                 res.append(copy.deepcopy(curr_res))
+#                 return
+
+#             if i>=len(candidates) or curr_sum > target:
+#                 return
+
+#             curr_res.append(candidates[i])
+#             dfs(i,curr_res, curr_sum+candidates[i])
+#             curr_res.pop()
+#             dfs(i+1,curr_res, curr_sum)
+
+        
+#         dfs(0,[], 0)
+
+#         return res

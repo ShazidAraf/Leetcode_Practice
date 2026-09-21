dd2663bab5fea6df8216338e00078c9c1f7f2b51
MD SHAZID ISLAM <misla048@ucr.edu>
2026-09-20 19:27:54 -0700

    Time: 33 ms (5.74%) | Memory: 12.3 MB (87.89%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,30 @@
+import copy
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+
+
+        def dfs(i,curr_res, curr_sum):
+
+
+            if curr_sum == target:
+                res.append(copy.deepcopy(curr_res))
+                return
+
+            if i>=len(candidates) or curr_sum > target:
+                return
+
+            curr_res.append(candidates[i])
+            dfs(i,curr_res, curr_sum+candidates[i])
+            curr_res.pop()
+            dfs(i+1,curr_res, curr_sum)
+
+        
+        dfs(0,[], 0)
+
+        return res

52591ac47a32d5c1217ad645096c4b4b3312fa91
MD SHAZID ISLAM <misla048@ucr.edu>
2026-09-26 06:21:35 -0700

    Time: 904 ms (40.79%) | Memory: 13 MB (37.39%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -7,66 +7,16 @@ class Solution(object):
         """
 
 
-        dp = [float('inf')]*(amount+1)
+        dp = [amount+1]*(amount+1)
 
         dp[0] = 0
 
-
         for i in range(1,amount+1):
-            for j in coins:
-                if i>=j:
-                    dp[i] = min(dp[i],1+dp[i-j])
+            for c in coins:
+                if i-c>=0:
+                    dp[i] = min(dp[i],1+dp[i-c])
 
-        # print(dp)
-
-
-        if dp[-1]==float('inf'):
+        if dp[-1]==amount+1:
             return -1
         else:
             return dp[-1]
-
-
-# class Solution(object):
-#     def coinChange(self, coins, amount):
-#         """
-#         :type coins: List[int]
-#         :type amount: int
-#         :rtype: int
-#         """
-
-#         self.res = -1
-#         coins.sort()
-
-
-#         def dfs(i,curr,total):
-
-
-#             if total==amount:
-
-#                 if self.res == -1:
-#                     self.res = len(curr)
-
-#                 else:
-#                     if len(curr)<self.res:
-#                         self.res = len(curr)
-
-#                 return
-
-            
-#             if i>len(coins)-1:
-#                 return
-
-            
-#             for j in range(i,len(coins)):
-
-#                 if total + coins[j]>amount:
-#                     break
-
-#                 curr.append(coins[j])
-#                 dfs(j,curr,total+coins[j])
-#                 curr.pop()
-
-
-#         dfs(0,[],0)
-
-#         return self.res
